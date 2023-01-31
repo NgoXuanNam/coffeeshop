25d1531e728f0e9c69c57828e62fdae5b280354d
NgoXuanNam <ngoxuannamit.vn@gmail.com>
2023-01-31 08:42:38 +0700

    update

--- assets/Js/main.js
+++ assets/Js/main.js
@@ -1,52 +1,60 @@
 let $ = document.querySelector.bind(document);
 let $$ = document.querySelectorAll.bind(document);
 // Xử lý Menu Icon
-let getNavBar = $('.navbar');
-let getMenuIcon = $('#menu-btn');
-let getOverlay = $('.overlay');
+let getNavBar = $(".navbar");
+let getMenuIcon = $("#menu-btn");
+let getOverlay = $(".overlay");
 
 getMenuIcon.onclick = () => {
-    getNavBar.classList.add('active');
-    getOverlay.classList.add('active');
-    getCartItem.classList.remove('active');
-    getSearch.classList.remove('active');
-}
+  getNavBar.classList.add("active");
+  getOverlay.classList.add("active");
+  getCartItem.classList.remove("active");
+  getSearch.classList.remove("active");
+};
 // Xử lý tắt menu
-let getCloseMenu = $('.navbar_close span');
-getCloseMenu.onclick = () =>{
-    getNavBar.classList.remove('active');
-    getOverlay.classList.remove('active');
-
+let getCloseMenu = $(".navbar_close span");
+getCloseMenu.onclick = () => {
+  getNavBar.classList.remove("active");
+  getOverlay.classList.remove("active");
 };
 
-getOverlay.onclick = function(){
-    getNavBar.classList.remove('active');
-    getOverlay.classList.remove('active');
+getOverlay.onclick = function () {
+  getNavBar.classList.remove("active");
+  getOverlay.classList.remove("active");
 };
-
 
 // Xử lý Cart Item
-let getCartItem = $('.cart-items-container');
-let getCartIcon = $('#cart-btn');
+let getCartItem = $(".cart-items-container");
+let getCartIcon = $("#cart-btn");
 
 getCartIcon.onclick = () => {
-    getCartItem.classList.toggle('active');
-    getNavBar.classList.remove('active');
-    getSearch.classList.remove('active');
-
-}
+  getCartItem.classList.toggle("active");
+  getNavBar.classList.remove("active");
+  getSearch.classList.remove("active");
+};
 // Xử lý Search Icon
-let getSearch = $('.search-form');
-let getSearchIcon = $('#search-btn');
+let btnScrollTop = document.querySelector(".onScrollTop");
+console.log(btnScrollTop);
+let getSearch = $(".search-form");
+let getSearchIcon = $("#search-btn");
 
 getSearchIcon.onclick = () => {
-    getSearch.classList.toggle('active');
-    getNavBar.classList.remove('active');
-    getCartItem.classList.remove('active');
-}
+  getSearch.classList.toggle("active");
+  getNavBar.classList.remove("active");
+  getCartItem.classList.remove("active");
+};
 window.onscroll = () => {
-    getNavBar.classList.remove('active');
-    getCartItem.classList.remove('active');
-    getSearch.classList.remove('active');
-    getOverlay.classList.remove('active')
-}
+  let scrollTop = document.documentElement.scrollTop;
+  getNavBar.classList.remove("active");
+  getCartItem.classList.remove("active");
+  getSearch.classList.remove("active");
+  getOverlay.classList.remove("active");
+  if (scrollTop > 200) {
+    btnScrollTop.classList.add("active");
+  } else {
+    btnScrollTop.classList.remove("active");
+  }
+};
+btnScrollTop.onclick = function () {
+  document.documentElement.scrollTop = 0;
+};
